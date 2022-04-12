9b7e25aa999d184e463f7b88433245a3b7f195bc
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-13 08:59:22 +0900

    Finish bare minimum requirements for recursion review

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,17 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var element = arguments[1] || document.body;
+  var result = [];
+  if (_.contains(element.classList, className)) {
+    result.push(element);
+  }
+  if (element.childElementCount > 0) {
+    _.forEach(element.childNodes, function(currentChild) {
+      result = result.concat(getElementsByClassName(className, currentChild));
+    });
+  }
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,40 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj !== 'object') {
+    return '' + obj;
+  } else if (obj === null) {
+    return 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    var result = '[';
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ',';
+      if (i === obj.length - 1) {
+        result = result.slice(0, result.length - 1);
+      }
+    }
+    result += ']';
+  } else {
+    var result = '{';
+    var allKeys = Object.keys(obj);
+    var counter = 0;
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      }
+      result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+      if (counter === allKeys.length - 1) {
+        result = result.slice(0, result.length - 1);
+      }
+      counter++;
+    }
+    result += '}';
+  }
+  return result;
   // your code goes here
 };
